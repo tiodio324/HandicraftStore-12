09352b30e1edaf259c01b4de891040afb0770d52
tiodio324 <nikita324smirnovvv@gmail.com>
2026-05-14 23:58:55 +0300

    ВКР

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,6 @@ import path from 'path'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/HandicraftStore-12/',
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
